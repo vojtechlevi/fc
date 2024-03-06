8c7008caf6126f35b4c9d19ade5da23ebfba3243
Levi Ekström <vojtechlevi1@hotmail.com>
2024-03-06 14:27:23 +0100

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,20 +11,20 @@ function About() {
     <>
       <div
         id="About"
-        className=" text-white w-full h-screen relative flex flex-col justify-center"
+        className=" text-white w-full h-screen relative flex flex-col"
       >
-        <div className=" relative bg-[rgb(30,30,30)] w-[90%] mx-auto flex my-10 p-10 rounded-[20px]">
+        <div className=" relative justify-center  w-[90%] mx-auto flex p-10 rounded-[20px]">
           <img
             src={placeholder}
             alt=""
             srcset=""
-            className=" w-[200px] hidden rounded-[20px] md:flex md:mx-5"
+            className=" w-[300px] hidden rounded-[20px] md:flex md:mx-5"
           />
           <div className=" flex flex-col">
             <h2 className=" font-bold text-center text-lg md:text-2xl md:text-left">
               Vilka är vi?
             </h2>
-            <p className=" mt-5 text-center text-xs max-w-[450px] md:max-w-[400px] md:text-left">
+            <p className=" mt-5 text-center text-sm max-w-[450px] md:max-w-[400px] md:text-left">
               Vårt fokus ligger starkt på restaurangkök, och genom åren har vi
               utvecklats till en mångsidig verksamhet som levererar till både
               stora och små företag inom alla sektorer. Med över tio års
@@ -35,44 +35,44 @@ function About() {
             </p>
           </div>
         </div>
-        <Test />
-        <div className=" bg-[rgb(30,30,30)] w-[90%] mx-auto flex justify-center items-center my-10 p-10 rounded-[20px]">
+        <div className=" bg-[rgb(30,30,30)] w-[90%] mx-auto flex justify-center items-center mb-10 py-10 px-1 rounded-[20px]">
           <div className="w-full text-center">
             <h2 className=" font-bold text-lg md:text-2xl">
               Varför ska ni välja oss?
             </h2>
             <div className="flex mt-5 w-full justify-around">
               <div>
-                <div class=" flex items-center justify-center text-center gap-4">
+                <div class=" flex items-center justify-center text-center">
                   <IoPricetagOutline className=" text-lg text-green-400" />
-                  <p className=" font-bold">Prisvärt</p>
+                  <p className=" font-bold ml-2">Prisvärt</p>
                 </div>
-                <p className="text-xs hidden mt-1 md:flex md:max-w-[200px]">
+                <p className="text-[10px] max-w-[100px] mt-1 md:flex md:max-w-[200px]">
                   Konkurrenskraftiga priser för kvalitativa produkter, stödjer
                   din verksamhet ekonomiskt.
                 </p>
               </div>
               <div className="">
-                <div class=" flex items-center justify-center text-center gap-4">
+                <div class=" flex items-center justify-center text-center">
                   <LuBarChartBig className=" text-lg text-red-400" />
-                  <p className=" font-bold">Flexibelt</p>
+                  <p className=" font-bold ml-1">Flexibelt</p>
                 </div>
-                <p className="text-xs hidden md:flex md:max-w-[200px]">
+                <p className="text-[10px] max-w-[100px] mt-1 md:flex md:max-w-[200px]">
                   Små eller stora, standard eller special. Vi anpassar oss efter
                   dina behov.
                 </p>
               </div>
               <div className="">
-                <div class=" flex items-center justify-center text-center gap-4">
+                <div class=" flex items-center justify-center text-center">
                   <RiCustomerService2Fill className=" text-lg text-orange-400" />
-                  <p className=" font-bold">Service</p>
+                  <p className=" font-bold ml-1">Service</p>
                 </div>
-                <p className="text-xs hidden md:flex md:max-w-[200px]">
+                <p className="text-[10px] max-w-[100px] mt-1 md:flex md:max-w-[200px]">
                   Små eller stora, standard eller special - vi anpassar oss
                   efter dina behov.
                 </p>
               </div>
             </div>
+            <Test />
           </div>
         </div>
       </div>
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -4,48 +4,48 @@ import { useInView } from "react-intersection-observer";
 function Test() {
   const [ref1, inView1] = useInView({ triggerOnce: false });
   return (
-    <div className=" bg-[rgb(30,30,30)] text-white py-6 flex justify-center items-center gap-4 md:gap-20 lg:gap-40">
+    <div className=" w-full bg-[rgb(30,30,30)] text-white py-6 flex justify-around items-center ">
       <div
         id="test"
         ref={ref1}
         className={
           inView1
-            ? "opacity-100 transition-all duration-[1s] delay-[200ms] "
-            : "opacity-0 transition-all duration-500"
+            ? "opacity-100 transition-all duration-[1s] delay-[200ms] min-w-[100px] md:min-w-[200px]  "
+            : "opacity-0 transition-all duration-500 "
         }
       >
         <p className=" font-bold text-md md:text-2xl">
           35<span className=" text-green-300 ml-1">+</span>
         </p>
-        <p className=" tracking-[2px] text-xs">Års erfarenhet</p>
+        <p className=" tracking-[2px] text-[10px]">Års erfarenhet</p>
       </div>
       <div
         id="test"
         ref={ref1}
         className={
           inView1
-            ? "opacity-100 transition-all duration-[1s] delay-[400ms]"
-            : "opacity-0 transition-all duration-500"
+            ? "opacity-100 transition-all duration-[1s] delay-[400ms] min-w-[100px] md:min-w-[200px]"
+            : "opacity-0 transition-all duration-500 "
         }
       >
         <p className=" font-bold text-md md:text-2xl">
           300<span className=" text-green-300 ml-1">+</span>
         </p>
-        <p className=" tracking-[2px] text-xs">Nöjda kunder</p>
+        <p className=" tracking-[2px] text-[10px]">Nöjda kunder</p>
       </div>
       <div
         id="test"
         ref={ref1}
         className={
           inView1
-            ? "opacity-100 transition-all duration-[1s] delay-[600ms]"
+            ? "opacity-100 transition-all duration-[1s] delay-[600ms] min-w-[100px] md:min-w-[200px]"
             : "opacity-0 transition-all duration-500"
         }
       >
         <p className=" font-bold text-md md:text-2xl">
           300<span className=" text-green-300 ml-1">+</span>
         </p>
-        <p className=" tracking-[2px] text-xs">Artiklar</p>
+        <p className=" tracking-[2px] text-[10px]">Artiklar</p>
       </div>
     </div>
   );
