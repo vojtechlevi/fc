4990bba3cba89d153d14dbcd384ca6834415ee78
Levi Ekström <vojtechlevi1@hotmail.com>
2024-11-18 10:10:37 +0100

    updated newsletter

--- client/src/components/Newsletter.jsx
+++ client/src/components/Newsletter.jsx
@@ -42,14 +42,21 @@ const Newsletter = () => {
 
   return (
     <section className="container h-screen snap-end justify-center flex items-center mx-auto my-10 max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="grid items-center gap-10 bg-black py-[15%] px-[6%] dark:bg-zinc-900 md:p-[6%] lg:grid-cols-2">
+      <div className="lg:h-1/2 relative grid items-center gap-10 py-[15%] px-[6%]  md:p-[6%] lg:grid-cols-2 overflow-hidden rounded-lg">
+        <img
+          src="https://images.unsplash.com/photo-1609869882537-6a860e45a0d6?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          srcset=""
+          className="absolute w-full h-full aspect-square object-cover object-center -z-10 rounded-lg translate-x-24"
+        />
+        <div className="absolute bg-black w-full h-full aspect-square object-cover object-center -z-20 rounded-lg"></div>
         <div>
-          <h3 className="text-5xl font-semibold text-white">
-            Join my newsletter
+          <h3 className="text-2xl font-semibold text-white">
+            Prenumerera på vårt nyhetsbrev
           </h3>
           <p className="mt-5 text-lg text-neutral-200">
-            Join my newsletter to get new posts before anyone else, I&apos;ll
-            send you an email with links to all of the articles.
+            Håll dig uppdaterad med de senaste nyheterna, inspirationen och
+            erbjudandena – direkt i din inkorg.
           </p>
         </div>
 
@@ -59,7 +66,7 @@ const Newsletter = () => {
               type="email"
               value={email}
               className="peer block w-full rounded-md border border-gray-300 bg-black py-3 pl-7 pr-12 text-white focus:border-white focus:outline-none focus:ring-1 focus:ring-white peer-invalid:text-pink-600 dark:border-zinc-500 dark:bg-zinc-900 dark:focus:ring-white sm:text-sm"
-              placeholder="Your Email"
+              placeholder="Din Mailadress"
               autoComplete="email"
               required
               onChange={(e) => setEmail(e.target.value)}
@@ -76,7 +83,7 @@ const Newsletter = () => {
                   <RiLoader5Fill className="w-8 animate-spin" />
                 </div>
               ) : (
-                "SUBSCRIBE"
+                "Prenumerera"
               )}
             </button>
           </div>
